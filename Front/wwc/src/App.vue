8c7edeff0d66df721b1434a78889cdd095e156e7
pplemover <paulimbest@naver.com>
2023-07-31 17:42:51 +0900

    [FE][S09P12A604-300] Feat: Header 스타일 추가

--- Front/wwc/src/App.vue
+++ Front/wwc/src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <!-- HEADER -->
+    <header>
+      header에용.
+      <div class="func_btn">button</div>
+    </header>
+
+    <!-- CONTENT -->
+    <div class="head-container">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+    
+  </div>
+  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
@@ -15,12 +30,30 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.head-container {
+  background-color: #F27059;
+}
+
+header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #FFCDAD;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-top: 1px solid #032c16;
+  border-bottom: 2px solid #032c16;
+  z-index: 9;
+}
+
+.header_menu {
+  position: fixed;
+  left: 130px;
+  width: 380px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
