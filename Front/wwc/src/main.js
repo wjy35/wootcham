5de9b9e517d3980e99928ea11b4c401c014cae35
pplemover <paulimbest@naver.com>
2023-08-03 12:50:41 +0900

    [FE] Chore: 폴더 삭제

--- Front/wwc/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './common/global.css'; 
-
-createApp(App).mount('#app')
--- Front/wwc/src/views/admin/AdminHome.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>관리자페이지</div>
-</template>
