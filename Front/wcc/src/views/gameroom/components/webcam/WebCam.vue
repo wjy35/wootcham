cc36b57f898816a3c2aeff26ae11ee1d1cc7846b
이주용 <jy23698@naver.com>
2023-08-04 13:36:08 +0900

    [FE] Merge: From FE_OpenVidu into FE_InterOne

--- Front/wcc/src/views/gameroom/components/webcam/WebCam.vue
+++ Front/wcc/src/views/gameroom/components/webcam/WebCam.vue
@@ -191,7 +191,8 @@ export default {
                 <img src="../../../../assets/images/WCC_logo.png">
             </div>
             <div class="player player2">
-                <img src="../../../../assets/images/WCC_logo.png">
+                <user-video :stream-manager="publisher" />
+                <!-- <img src="../../../../assets/images/WCC_logo.png"> -->
             </div>
         </div>
         <div class="row1">
@@ -199,7 +200,8 @@ export default {
                 <img src="../../../../assets/images/WCC_logo.png">
             </div>
             <div class="player player4">
-                <img src="../../../../assets/images/WCC_logo.png">
+                <user-video :stream-manager="publisher" />
+                <!-- <img src="../../../../assets/images/WCC_logo.png"> -->
             </div>
             <div class="player player5">
                 <img src="../../../../assets/images/WCC_logo.png">
@@ -212,9 +214,151 @@ export default {
 </template>
 
 <script>
-export default {
+import axios from "axios";
+import { OpenVidu } from "openvidu-browser";
+import UserVideo from "./components/UserVideo";
 
-}
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+const APPLICATION_SERVER_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000/';
+
+export default {
+    name: "App",
+
+    components: {
+        UserVideo,
+    },
+
+    data() {
+        return {
+            // OpenVidu objects
+            OV: undefined,
+            session: undefined,
+            mainStreamManager: undefined,
+            publisher: undefined,
+            subscribers: [],
+
+            // Join form
+            mySessionId: "SessionA",
+            myUserName: "Participant" + Math.floor(Math.random() * 100),
+        };
+        
+    },
+
+    mounted() {
+        this.joinSession();
+    },
+
+    methods: {
+        joinSession() {
+            // --- 1) Get an OpenVidu object ---
+            this.OV = new OpenVidu();
+
+            // --- 2) Init a session ---
+            this.session = this.OV.initSession();
+
+            // --- 3) Specify the actions when events take place in the session ---
+
+            // On every new Stream received...
+            this.session.on("streamCreated", ({ stream }) => {
+                const subscriber = this.session.subscribe(stream);
+                this.subscribers.push(subscriber);
+            });
+
+            // On every Stream destroyed...
+            this.session.on("streamDestroyed", ({ stream }) => {
+                const index = this.subscribers.indexOf(stream.streamManager, 0);
+                if (index >= 0) {
+                    this.subscribers.splice(index, 1);
+                }
+            });
+
+            // On every asynchronous exception...
+            this.session.on("exception", ({ exception }) => {
+                console.warn(exception);
+            });
+
+            // --- 4) Connect to the session with a valid user token ---
+
+            // Get a token from the OpenVidu deployment
+            this.getToken(this.mySessionId).then((token) => {
+
+                // First param is the token. Second param can be retrieved by every user on event
+                // 'streamCreated' (property Stream.connection.data), and will be appended to DOM as the user's nickname
+                this.session.connect(token, { clientData: this.myUserName })
+                    .then(() => {
+
+                        // --- 5) Get your own camera stream with the desired properties ---
+
+                        // Init a publisher passing undefined as targetElement (we don't want OpenVidu to insert a video
+                        // element: we will manage it on our own) and with the desired properties
+                        let publisher = this.OV.initPublisher(undefined, {
+                            audioSource: undefined, // The source of audio. If undefined default microphone
+                            videoSource: undefined, // The source of video. If undefined default webcam
+                            publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+                            publishVideo: true, // Whether you want to start publishing with your video enabled or not
+                            resolution: "640x480", // The resolution of your video
+                            frameRate: 30, // The frame rate of your video
+                            insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
+                            mirror: false, // Whether to mirror your local video or not
+                        });
+
+                        // Set the main video in the page to display our webcam and store our Publisher
+                        this.mainStreamManager = publisher;
+                        this.publisher = publisher;
+
+                        // --- 6) Publish your stream ---
+
+                        this.session.publish(this.publisher);
+                    })
+                    .catch((error) => {
+                        console.log("There was an error connecting to the session:", error.code, error.message);
+                    });
+            });
+
+            window.addEventListener("beforeunload", this.leaveSession);
+        },
+
+        leaveSession() {
+            // --- 7) Leave the session by calling 'disconnect' method over the Session object ---
+            if (this.session) this.session.disconnect();
+
+            // Empty all properties...
+            this.session = undefined;
+            this.mainStreamManager = undefined;
+            this.publisher = undefined;
+            this.subscribers = [];
+            this.OV = undefined;
+
+            // Remove beforeunload listener
+            window.removeEventListener("beforeunload", this.leaveSession);
+        },
+
+        updateMainVideoStreamManager(stream) {
+            if (this.mainStreamManager === stream) return;
+            this.mainStreamManager = stream;
+        },
+
+        async getToken(mySessionId) {
+            const sessionId = await this.createSession(mySessionId);
+            return await this.createToken(sessionId);
+        },
+
+        async createSession(sessionId) {
+            const response = await axios.post(APPLICATION_SERVER_URL + 'api/sessions', { customSessionId: sessionId }, {
+                headers: { 'Content-Type': 'application/json', },
+            });
+            return response.data; // The sessionId
+        },
+
+        async createToken(sessionId) {
+            const response = await axios.post(APPLICATION_SERVER_URL + 'api/sessions/' + sessionId + '/connections', {}, {
+                headers: { 'Content-Type': 'application/json', },
+            });
+            return response.data; // The token
+        },
+    },
+};
 </script>
 
 <style>
