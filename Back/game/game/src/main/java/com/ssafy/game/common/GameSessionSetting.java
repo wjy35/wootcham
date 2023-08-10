5ea5687c82d6626225d6613119194c22978ebbe3
HI0516 <wangsun7@naver.com>
2023-08-10 10:22:47 +0900

    [BE][S09P12A604-431] Refactor: 최대 게임 인원 수정

--- Back/game/game/src/main/java/com/ssafy/game/common/GameSessionSetting.java
+++ Back/game/game/src/main/java/com/ssafy/game/common/GameSessionSetting.java
@@ -3,7 +3,7 @@ package com.ssafy.game.common;
 public class GameSessionSetting {
     public static final int MAX_GAME_WAIT_SECOND = 5;
     public static final int MAX_PICK_TOPIC_TYPE_SECOND = 10;
-    public static final int MAX_PICK_TOPIC_KEYWORD_SECOND = 10;
+    public static final int MAX_PICK_TOPIC_KEYWORD_SECOND = 15;
     public static final int MAX_PREPARE_PRESENT_SECOND = 10;
-    public static final int MAX_PRESENT_SECOND = 10;
+    public static final int MAX_PRESENT_SECOND = 15;
 }
