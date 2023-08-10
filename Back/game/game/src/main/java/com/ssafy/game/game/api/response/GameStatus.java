94ea8c9b59f133238f1ed05a3781c3885d31a961
왕준영 <wangsun7@naver.com>
2023-08-10 10:17:22 +0900

    [BE][S09P12A604-431] Merge: BE_GameProcess into BE_Game

--- Back/game/game/src/main/java/com/ssafy/game/game/api/response/GameStatus.java
+++ Back/game/game/src/main/java/com/ssafy/game/game/api/response/GameStatus.java
@@ -2,7 +2,7 @@ package com.ssafy.game.game.api.response;
 
 public class GameStatus {
     public static final int WAIT_GAME_START = 1;
-    public static final int ORDER_GAMEMEMBER = 2;
+    public static final int ROUND_SETTING = 2;
     public static final int PICK_TOPIC_TYPE = 3;
     public static final int PICK_TOPIC_KEYWORD = 4;
     public static final int PREPARE_PRESENT = 5;
