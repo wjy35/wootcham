5d1904b2f30fa8570b92bd424df1a38a713dd18b
unknown <dhrtnqls0535@naver.com>
2023-08-13 01:22:21 +0900

    [FE][S09P12A604-620] Refactor: 로그인 Response 수정

--- Front/wcc/src/api/index.js
+++ Front/wcc/src/api/index.js
@@ -3,8 +3,8 @@ import axios from "axios";
 // axios 객체 생성
 export default axios.create({
   // baseURL: "https://i9a604.p.ssafy.io",
-  // baseURL: "https://wootcham.com/api",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://wootcham.com/api",
+  // baseURL: "http://localhost:8080",
   headers: {
     "Content-type": "application/json",
   },
