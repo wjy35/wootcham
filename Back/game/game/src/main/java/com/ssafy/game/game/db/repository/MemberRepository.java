d49a43a2e53b6e3b805380f2ba98f6901ca834f0
HI0516 <wangsun7@naver.com>
2023-08-16 10:42:07 +0900

    [BE][S09P12A604-519] Fix: 발표 오류 버그 수정

--- Back/game/game/src/main/java/com/ssafy/game/game/db/repository/MemberRepository.java
+++ Back/game/game/src/main/java/com/ssafy/game/game/db/repository/MemberRepository.java
@@ -3,12 +3,12 @@ package com.ssafy.game.game.db.repository;
 import com.ssafy.game.game.db.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Integer> {
     @Procedure("pointProc")
-    int updateRankPoint(String nickname_input,Integer point_change);
+    int updateRankPoint(@Param("nickname_input") String nickname,@Param("point_change") Integer point);
 }
