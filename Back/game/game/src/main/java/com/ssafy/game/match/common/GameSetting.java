9b32fed0fe78b438a7f43e439458bb637b3ddc12
HI0516 <wangsun7@naver.com>
2023-08-17 18:13:22 +0900

    [BE][S09P12A604-754] Refactor: 발표 인원 수정

--- Back/game/game/src/main/java/com/ssafy/game/match/common/GameSetting.java
+++ Back/game/game/src/main/java/com/ssafy/game/match/common/GameSetting.java
@@ -4,7 +4,7 @@ public class GameSetting {
     public static final Integer MAX_GAMEMEMBER_COUNT = 6;
     public static final Integer MAX_GAMEMEMBER_ENTER_WAIT_SECOND = 10;
     public static final Integer MAX_GAME_LOAD_SECOND = 30;
-    public static final Integer MAX_GAME_ROOM_COUNT = 100s;
+    public static final Integer MAX_GAME_ROOM_COUNT = 100;
 
     public static final Integer ROUND_COUNT = 1;
 }
