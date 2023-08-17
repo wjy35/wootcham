060e470fbbbe5e2ca4760f62af4abb06ca88c96b
seokju <tjrwn1247@naver.com>
2023-08-18 00:32:05 +0900

    [BE] Chore: Security 수정

--- Back/api/wcc/src/main/java/com/ssafy/wcc/common/config/SecurityConfig.java
+++ Back/api/wcc/src/main/java/com/ssafy/wcc/common/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("https://wootcham.com");
         config.addAllowedOrigin("http://70.12.247.106:3000");
+        config.addAllowedOrigin("http://192.168.55.57:3000");
         config.addAllowedOrigin("http://localhost:3000");
         config.setAllowedMethods(Arrays.asList("HEAD","POST","GET", "PUT", "DELETE", "OPTION"));
         config.setAllowedHeaders(Arrays.asList("*"));
