a65f95f50b46972d65d2f274eb87f0cb722161ba
HI0516 <wangsun7@naver.com>
2023-08-18 10:13:31 +0900

    [BE][S09P12A604-754] Refactor: 발표 인원 수정

--- Back/game/game/src/main/java/com/ssafy/game/match/common/WebSocketEventListener.java
+++ Back/game/game/src/main/java/com/ssafy/game/match/common/WebSocketEventListener.java
@@ -1,11 +1,9 @@
 package com.ssafy.game.match.common;
 
-import com.ssafy.game.game.api.service.GameService;
 import com.ssafy.game.match.api.service.MatchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
